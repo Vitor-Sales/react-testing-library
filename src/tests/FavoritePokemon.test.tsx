2d2f41e896fba11f26c4d3af71d8ff2cb36ba1f8
Vitor Sales <vitormalisa@gmail.com>
2024-02-21 12:46:33 -0300

    Feat: task 3 tests done

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -2,6 +2,32 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('Testando página favorite pokémons: Se mostra a mensagem "No favorite pokémons found", Se ao favoritar pokémons mostram apenas os favoritados', () => {
-  renderWithRouter;
+describe('Testando página favorite pokémons', () => {
+  test('Se mostra a mensagem "No favorite pokémons found"', () => {
+    // mock
+    // render
+    renderWithRouter(<App />, { route: '/favorites' });
+
+    expect(screen.getByText(/No favorite pokémon found/i)).toBeInTheDocument();
+  });
+
+  test('Se ao favoritar pokémons mostram apenas os favoritados', async () => {
+    // render
+    const { user } = renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    await user.click(moreDetails);
+
+    const checkbox = screen.getByRole('checkbox');
+    await user.click(checkbox);
+
+    expect(checkbox).toBeChecked();
+
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    await user.click(linkFavorite);
+
+    const pokemon = screen.getByText(/pikachu/i);
+
+    expect(pokemon).toBeInTheDocument();
+  });
 });
