b7e16ec9d76dcfe6266a12f5cbe89fe4cac194c2
Vitor Sales <vitormalisa@gmail.com>
2024-02-21 12:55:38 -0300

    Feat: task 4 tests done

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente NotFound', () => {
+  test('Se a página contem um heading h2 com o texto "Page requested not found', () => {
+    renderWithRouter(<App />, { route: '/whatever' });
+
+    const heading = screen.getByRole('heading', { name: /page requested not found/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Teste se a página mostra a imagem com o texto alternativo Clefairy pushing buttons randomly with text I have no idea what i'm doing", () => {
+    renderWithRouter(<App />, { route: '/whatever' });
+
+    const imgAlt = screen.getByAltText(/Clefairy pushing buttons randomly with text I have no idea what i'm doing/i);
+
+    expect(imgAlt).toBeInTheDocument();
+  });
+});
